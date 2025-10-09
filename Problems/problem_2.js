90f1775ecdcdf6f2b66b881a45c62054af0687ec
CseRashed <cserashedul@gmail.com>
2025-10-09 14:43:23 +0600

    solving string method problem_2

---
+++ Problems/problem_2.js
@@ -0,0 +1,12 @@
+// Reverse a String
+
+let str = "Rashedul";
+let reversed = "";
+
+for (let i = str.length-1 ; i >= 0; i--) {
+  reversed += str[i];
+}
+console.log(reversed);
+
+// Sample Input: "World"
+// Sample Output: "dlroW"
